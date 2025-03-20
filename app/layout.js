97e185d183e91e3c072077a645d1eefca6e21fbb
MostakemHossain <mostakememon123@gmail.com>
2025-03-20 09:48:23 +0600

    clerk setup

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -13,12 +15,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${inter.className} `}
       >
+        <Header/>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
